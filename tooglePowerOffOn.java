9925073b55215ccf8853bae3d1f943768959efde
bastmre44 <134813711+bastmre44@users.noreply.github.com>
2024-01-23 22:24:17 -0600

    Update tooglePowerOffOn.java

--- tooglePowerOffOn.java
+++ tooglePowerOffOn.java
@@ -23,3 +23,43 @@ public class tooglePowerOffOn {
         radio.getStateAMFM();
     }
 }
+public void NextFrequency(){
+    if (isFM){
+        currentFrequency += 0.2; /// para incrementar la frecuencia 
+    if (currentFrequency > 108.of){
+        currentFrequency = 87.5;
+    }
+    } else {
+        currentFrequency += 10.of;/// para incrementar la frecuencia AM
+    if (currentFrequency > 1700.of){
+        currentFrequency = 530.of;  /// para volver a la frecuencia inicail 
+
+    }
+    }    
+    System.out.println("Frecuencia actual: " + currentFrequency);
+    }
+
+
+
+    public void PreviousFrequency(){
+        if (isFM){
+            currentFrequency -= 0.2f; /// para decrementar la frecuencia|
+        if (currentFrequency < 87.5){
+            currentFrequency = 108.of;
+
+        }
+        } else {
+            currentFrequency += 10.of;/// para decrementar 
+        if (currentFrequency < 530.of){
+            currentFrequency = 1700.of;  /// para volver la frecuencia
+        }
+    } else {
+        currentFrequency -= 10.0f; // Decrementa la frecuencia AM por 10 (puedes ajustar este valor)
+        if (currentFrequency < 530.0f) {
+            currentFrequency = 1700.0f; // Vuelve a la frecuencia máxima si es menor que el límite inferior
+        }
+    }
+    System.out.println("Frecuencia actual: " + currentFrequency);
+}
+    
+
